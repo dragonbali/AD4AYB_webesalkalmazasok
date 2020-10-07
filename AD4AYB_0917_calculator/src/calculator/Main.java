6bac80d17e1bf0ea4dc0ac1e9f9f87ddd5d6e45e
Hornyák Balázs <kicsikis@gmail.com>
2020-10-07 20:04:32 +0200

    summation added

---
+++ AD4AYB_0917_calculator/src/calculator/Main.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 2;
+		int x;
+		x = a + b;
+		
+		System.out.println(x);
+
+	}
+
+}
