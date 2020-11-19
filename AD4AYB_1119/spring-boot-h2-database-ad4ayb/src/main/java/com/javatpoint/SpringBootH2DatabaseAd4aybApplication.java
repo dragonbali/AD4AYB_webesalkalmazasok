c66f7fc01f832aa5a2e5729e676fe38cd55d4a66
Hornyák Balázs <kicsikis@gmail.com>
2020-11-19 11:30:30 +0100

    1015 copy, 1119 project added but not done

---
+++ AD4AYB_1119/spring-boot-h2-database-ad4ayb/src/main/java/com/javatpoint/SpringBootH2DatabaseAd4aybApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2DatabaseAd4aybApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2DatabaseAd4aybApplication.class, args);
+	}
+
+}
---
+++ AD4AYB_1119/spring-boot-h2-database-ad4ayb/src/test/java/com/javatpoint/springbooth2databasead4ayb/SpringBootH2DatabaseAd4aybApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatpoint.springbooth2databasead4ayb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootH2DatabaseAd4aybApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
