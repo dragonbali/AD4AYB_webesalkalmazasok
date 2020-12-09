fd470db02e1bf1b2128eefdcae2c66faa5c5349f
Hornyák Balázs <kicsikis@gmail.com>
2020-12-09 16:14:57 +0100

    design added

--- SemTaskAd4ayb/src/main/java/com/ad4ayb/semtaskad4ayb/config/SecurityConfig.java
+++ SemTaskAd4ayb/src/main/java/com/ad4ayb/semtaskad4ayb/config/SecurityConfig.java
@@ -16,16 +16,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-                .authorizeRequests()
-                    .antMatchers("/").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
-                .logout()
-                    .permitAll();
+						.authorizeRequests()
+							.anyRequest().authenticated()
+							.and()
+						.formLogin()
+							.loginPage("/login")
+							.permitAll()
+							.and()
+						.logout()
+							.permitAll();
     }
 
     @Bean
